2feccd8619c78bb14e74a8659ed30f9d2803e4b0
toney99 <13699870007@163.com>
2016-09-18 20:27:08 +0800

    fix some bug, save url

--- get_allnode.py
+++ get_allnode.py
@@ -3,8 +3,10 @@ import requests
 from bs4 import BeautifulSoup
 import json
 import re
+from random import random,randint
 # import get_product,save_asin, get_two_menu
 import mongo_db
+import time
 
 url = "https://www.amazon.com/gp/site-directory/"
 # ch_url = "https://www.amazon.com/home-automation-smarthome/b/ref=sd_allcat_homaut?ie=UTF8&node=6563140011"
@@ -18,6 +20,7 @@ headers = {
 	'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.84 Safari/537.36',
 }
 
+
 # 读取https://www.amazon.com/gp/site-directory/中的所有的二级菜单的地址
 # 根据二级菜单的地址获取该菜单下所有子节点的url地址
 # 每个二级菜单下的子菜单位于不同的位置，需要按每个菜单的特点进行解析
@@ -25,34 +28,39 @@ def get_allnode(ch_url, name, parent_name):
 	try:
 		r = requests.get(ch_url, headers=headers)
 		soup = BeautifulSoup(r.text, 'lxml')
+		# print soup.select('div#refinements')
+		# print soup.select('div.shoppingEngineSectionHeaders')
+		# print soup.select('div.categoryRefinementsSection')
+		# return
 	except:
 		print 'requests error'
-		pass
-	# res = soup.select('div.categoryRefinementsSection > ul > li > a')
-	# res = soup.select('ul.forExpando > li > a')
+		return
 	selector = [
-		'div.categoryRefinementsSection > ul > li'
+		'div.categoryRefinementsSection > ul > li',
 		'ul.refinementNodeChildren > li > a',
 		'ol.a-carousel > li > a',
 	]
+	vals = {
+		'parent_name':parent_name,
+		'name':name,
+		'url':ch_url,
+	}
+	sheet_tab = mongo_db.mongo_connect('amazon', 'select_url')
 	for s in selector:
 		node = soup.select(s)
 		if node:
-			# for n in node:
-			# 	print n['href']
 			print name
+			vals['soup'] = True
+			sheet_tab.insert(vals)
 			break
 		else:
 			pass
 	if not node:
 		print parent_name, name, ch_url
-	# 	r = requests.get(ch_url, headers=headers)
-	# 	soup = BeautifulSoup(r.text, 'lxml')
-	# 	if not soup.select('div#refinements'):
-	# 		# print '#??#' * 40
-	# 		print ch_url
-	# 		# print '#??#' * 40
-	# 	return
+		print '****' * 40
+		vals['soup'] = False
+		sheet_tab.insert(vals)
+		# time.sleep(30)
 	# for r in node_children:
 	# 	url =  base + r['href']
 	# 	name = r.find('span') and r.find('span').text or ''
@@ -69,9 +77,14 @@ def check_direct_name(name=''):
 		res = sheet_tab.find()
 	for r in res:
 		# print '****' * 40
-		# print res.count(), r['parent_name'], r['url']
+		# print res.count(), r['parent_name'], r['url'].split('/')
+		s_num = str(randint(100, 999)) + '-' + str(randint(1000000, 9999999)) + '-' + str(randint(1000000, 9999999))
+		s = r['url'].split('/')
+		# s[-1] = s_num + "?" + str(s[-1].split('?')[-1])
+		s[-1] = "?" + str(s[-1].split('?')[-1])
+		url = "/".join(s)
 		# print '****' * 40
-		get_allnode(r['url'], r['name'], r['parent_name'])
+		get_allnode(url, r['name'], r['parent_name'])
 
 # check_direct_name('Home, Garden & Tools')
 parent_name_list = [
@@ -86,4 +99,5 @@ parent_name_list = [
 ]
 for name in parent_name_list:
 	check_direct_name(name)
-
+# url = "https://www.amazon.com/Camera-Photo-Film-Canon-Sony/b/ref=sd_allcat_p/?ie=UTF8&node=502394"
+# get_allnode(url, 'name', 'parent_name')
